535cc7c70d2515210e96ff8b656b9570eb239b4c
Alejandro <alejandropcj@gmail.com>
2026-02-12 21:39:26 -0500

    feat: Se realiza implementacion del input inicial de acuerdo a los disenios en figma

---
+++ frontend/curatio/src/features/users/components/UserForm.jsx
@@ -0,0 +1,18 @@
+import Input from "../../../shared/components/Input";
+
+export default function UserForm() {
+  return (
+    <div>
+      {/*Formulario para crear el usuario */}
+
+      <form>
+        <Input label="Nombre y Apellidos" 
+                placeholder="Juan Rivera Grisales"
+                
+        
+        >
+        </Input>
+      </form>
+    </div>
+  );
+}
