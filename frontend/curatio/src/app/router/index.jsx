7c69ebbf84c442ff2c8b9e3b58445f3ff298b87f
unknown <jhonierstiven1807@gmail.com>
2026-02-25 19:10:34 -0500

    feat: componentes sin react router y sin nabvar

---
+++ frontend/curatio/src/app/router/index.jsx
@@ -0,0 +1,29 @@
+import {createBrowserRouter} from "react-router-dom"
+import MainLayout from "@/shared/layout/MainLayout"
+
+const router = createBrowserRouter([
+    {
+            path: "/",
+            element:<MainLayout/>,
+            children: [
+                {
+                    index: true,
+                    element: <h1 className="p-4">Inicio</h1>
+                },
+                {
+                    path:"cursos",
+                    element: <h1 className="p-4">Cursos</h1>
+                },
+                {
+                    path:"contacto",
+                    element: <h1 className="p-4">Contacto</h1>
+                },
+                {
+                    path:"videos",
+                    element: <h1 className="p-4">Videos</h1>
+                },
+        ]
+    }
+])
+
+export default router;
